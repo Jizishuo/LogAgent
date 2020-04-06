74a6deafe57f6530114e6e87bc550740d585b501
Jizishuo <jizishuo@qq.com>
2020-04-06 20:54:32 +0800

    0406

--- config/config.go
+++ config/config.go
@@ -3,10 +3,30 @@ package config
 // KafkaCnf 配置
 type KafkaConf struct {
 	Address string `ini:"address"`
-	Topic string `ini:"topic"`
-	MaxChan int `ini:"maxchan"`
+	Topic   string `ini:"topic"`
+	MaxChan int    `ini:"maxchan"`
 }
 
+type TaillogCof struct {
+	FilePth string `ini:"filePath"`
+}
 
+type Etcd struct {
+	Address string `ini:"address"`
+	Timeout int    `ini:"timeout"`
+	Key     string `ini:"key"`
+}
 
+type ElasticSearch struct {
+	Address string `ini:"address"`
+	MaxChan int    `ini:"maxchan"`
+	Index   string `ini:"index"`
+	Type    string `ini:"type"`
+}
 
+type Conf struct {
+	KafkaConf     `ini:"kafka"`
+	TaillogCof    `ini:"taillog"`
+	Etcd          `ini:"etcd"`
+	ElasticSearch `ini:"elasticsearch"`
+}
