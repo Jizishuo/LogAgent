96e8bb28cbc8b34846a19c2bda6de861b6dfcf24
Jizishuo <jizishuo@qq.com>
2020-04-08 15:18:41 +0800

    common

--- common/common.go
+++ common/common.go
@@ -1,3 +1,17 @@
 package common
 
+import (
+	"net"
+	"strings"
+)
 
+func GetOutboundIP() (ip string, err error) {
+	conn, err := net.Dail("udp", "0.0.0.0:8888")
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	ip = strings.Split(localAddr.Ip.String(), ":")[0]
+	return
+}
--- kafka/kafka.go
+++ kafka/kafka.go
@@ -4,55 +4,57 @@ import (
 	"LogAgent/elasticsearch"
 	"log"
 	"time"
+
 	"github.com/Shopify/sarama"
 )
 
 var (
 	client sarama.SyncProducer
-	// logchan log信息
+	// Logchan log信息
 	Logchan chan *LogData
 )
 
+// LogData 日志信息结构体
 type LogData struct {
 	Topic string
-	Data string
+	Data  string
 }
 
-// 初始化kafka
+// Init 初始化kafka
 func Init(addrs []string, maxChan int) (err error) {
-	config := sarama.NewConFig()
-	config.Producer.RequierAcks = sarama.WaitForAll
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
 	config.Producer.Partitioner = sarama.NewRandomPartitioner
 	config.Producer.Return.Successes = true
 	client, err = sarama.NewSyncProducer(addrs, config)
-	// 初始化日志通道
-	logchan = make(chan *LogData, maxChan)
-	//启动一个协程从通道中娶日志并发送给kafka
+	//初始化日志通道
+	Logchan = make(chan *LogData, maxChan)
+	//启动一个协程从通道中取日志并发送给kafka
 	go getLogByChan()
 	return
-
 }
-// 从通道中娶日志并发送给kafka
+
+// 从通道中取出log并发送到kafka
 func getLogByChan() {
 	for {
 		select {
-		case logInfo := <- Logchan:
+		case logInfo := <-Logchan:
 			SendMessag2Kafka(logInfo.Topic, logInfo.Data)
 		default:
-			time.Sleep(time.Microsecond*50)
+			time.Sleep(time.Millisecond * 50)
 		}
 	}
 }
 
-// SendMessag2Kafka 发送消息给kafka
+// SendMessag2Kafka 发送信息到kafka
 func SendMessag2Kafka(topic string, message string) {
-	msg := &sarama.ProducerMessage{Topic:topic, Value:sarama.StringEncoder(message)}
+	msg := &sarama.ProducerMessage{Topic: topic, Value: sarama.StringEncoder(message)}
 	_, _, err := client.SendMessage(msg)
 	if err != nil {
-		log.Printf("senf message failed err: %v", err)
+		log.Printf("send message failed err : %v", err)
 		return
 	}
-	log.Printf("Topic: %s, Message: %s\n", topic, message)
+	log.Printf("Topic: %s , Message : %s\n", topic, message)
 }
 
 // ConsumeMessage 消费kafka数据
@@ -61,23 +63,23 @@ func ConsumeMessage(address, topic string) error {
 	if err != nil {
 		return err
 	}
-	partitionList, err := consumer.Partitions("test0001")
+	partitionList, err := consumer.Partitions("test01")
 	if err != nil {
 		return err
 	}
-	log.Println("分区列表:", partitionList)
-	for partition := range partitionList {
-		partitionConsume, err := consumer.ConsumePartition(topic, int32(partition), sarama.OffsetNewest)
+	log.Println("分区列表：", partitionList)
+	for partion := range partitionList {
+		partitionConsume, err := consumer.ConsumePartition(topic, int32(partion), sarama.OffsetNewest)
 		if err != nil {
 			return err
 		}
-
-		go func(sarama.PartitionConsume.Message) {
-			// 发送数据到队列
-			for mes := range partitionConsume.Message() {
+		// defer partitionConsume.AsyncClose()
+		go func(sarama.PartitionConsumer) {
+			for msg := range partitionConsume.Messages() {
+				//发送数据到队列
 				elasticsearch.SendMessage2Chan(&elasticsearch.LogInfo{
-					Log:  string(mes.Value),
-					Time: time.Now().Format("2020-01-01 01:01:01"),
+					Log:  string(msg.Value),
+					Time: time.Now().Format("2006-01-02 15:04:05"),
 				})
 			}
 		}(partitionConsume)
